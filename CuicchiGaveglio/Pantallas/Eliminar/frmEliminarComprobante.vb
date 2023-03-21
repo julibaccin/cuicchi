c0992ec69b142bf5843303461ba3ebcd4ab5aa0e
Julian Baccin <julibaccin01@gmail.com>
2023-03-21 14:40:59 -0300

    feat: eliminar comprobante

--- CuicchiGaveglio/Pantallas/Eliminar/frmEliminarComprobante.vb
+++ CuicchiGaveglio/Pantallas/Eliminar/frmEliminarComprobante.vb
@@ -2,8 +2,15 @@
 Imports CapaDatos
 
 Public Class FrmEliminarComprobante
-
+    ReadOnly Control As New ClsComprobantes()
     Private Sub TxtIdAlta_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtIdAlta.KeyPress
         SoloNumero(e)
+    End Sub
+
+    Private Sub btnEliminarAltaComprobante_Click(sender As Object, e As EventArgs) Handles btnEliminarAltaComprobante.Click
+        Dim response = Control.EliminarComprobante(txtIdAlta.Text)
+        If (response = 1) Then
+            MsgBox("Comprobante eliminado con exito")
+        End If
     End Sub
 End Class
